13cfb6b560fa824e206134991f10292eb69c0b47
Amine Abbassi <Contact.amine.abbassi@gmail.com>
2025-05-25 20:17:29 +0100

    make sure the first argument is a number

---
+++ 5-to_integer.js
@@ -0,0 +1,10 @@
+const firstArgument = process.argv[2];
+const num = Number(firstArgument);
+
+
+if (Number.isInteger(num)) {
+    console.log("My number : " + num)
+    
+} else {
+    console.log("Not a Number")
+}
