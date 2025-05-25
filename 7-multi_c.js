a53ee14d36a7f9a43d0038b16fc26bd998304e49
Amine Abbassi <Contact.amine.abbassi@gmail.com>
2025-05-25 20:45:49 +0100

    C is fun

---
+++ 7-multi_c.js
@@ -0,0 +1,12 @@
+const firstArgument = process.argv[2];
+const num = Number(firstArgument);
+
+if (Number.isInteger(num)) {
+    for(let i=0;i<num;i++){
+
+        console.log("C is fun")
+    }
+
+} else {
+    console.log("Missing number of occurrences")
+}
