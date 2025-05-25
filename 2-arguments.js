3773176dec233e2462ced8eb680c2f890bafef6f
Amine Abbassi <Contact.amine.abbassi@gmail.com>
2025-05-25 17:46:14 +0100

    added arguments file

---
+++ 2-arguments.js
@@ -0,0 +1,12 @@
+let argumentPassed;
+if (argumentPassed==0) {
+    console.log("No argument");
+} 
+
+else if (argumentPassed==1) { 
+    console.log("Argument found");
+}
+
+else {
+    console.log("Arguments found");
+}
