e081700a6470a5862d7b36ec3c380664a1642399
Amine Abbassi <Contact.amine.abbassi@gmail.com>
2025-05-25 18:16:49 +0100

    added the argument algorithm

--- 2-arguments.js
+++ 2-arguments.js
@@ -1,12 +1,15 @@
-let argumentPassed;
-if (argumentPassed==0) {
+const argumentPassed = process.argv.length-2;
+
+if (argumentPassed == 0) {
     console.log("No argument");
 } 
 
-else if (argumentPassed==1) { 
+else if (argumentPassed == 1) { 
     console.log("Argument found");
 }
 
 else {
     console.log("Arguments found");
 }
+
+
