a1b245b19338012417f10f9f4b88dffd4b0136f4
Amine Abbassi <Contact.amine.abbassi@gmail.com>
2025-05-25 20:56:28 +0100

    C is fun

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -1,5 +1,23 @@
+
 const firstArgument = process.argv[2];
 const num = Number(firstArgument);
+
+if (Number.isInteger(num) && num >= 0) {
+    let output = "";
+    for (let i = 0; i < num; i++) {
+        output += "C is fun\n";
+    }
+    console.log(output.trim());
+} else {
+    console.log("Missing number of occurrences");
+}
+
+
+
+/* 
+const firstArgument = process.argv[2];
+const num = Number(firstArgument);
+
 
 if (Number.isInteger(num)) {
     for(let i=0;i<num;i++){
@@ -9,4 +27,4 @@ if (Number.isInteger(num)) {
 
 else {
     console.log("Missing number of occurrences")
-}
+} */
