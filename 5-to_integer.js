a9f076c471ec24efd14deaee415007c2538e29e0
Amine Abbassi <Contact.amine.abbassi@gmail.com>
2025-05-25 20:19:11 +0100

    make sure the first argument is a number

--- 5-to_integer.js
+++ 5-to_integer.js
@@ -3,8 +3,8 @@ const num = Number(firstArgument);
 
 
 if (Number.isInteger(num)) {
-    console.log("My number : " + num)
+    console.log("My number :" + num)
     
 } else {
-    console.log("Not a Number")
+    console.log("Not a number")
 }
